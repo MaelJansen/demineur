14d4c8925b51c60f38302b1e2ab0092c96258268
Mael Jansen <maeljansen2003@gmail.com>
2022-11-21 17:21:49 +0100

    Suppression de code mort

--- demineur/src/EcouteurFenetre.java
+++ demineur/src/EcouteurFenetre.java
@@ -7,18 +7,4 @@ public class EcouteurFenetre extends WindowAdapter{
 	public EcouteurFenetre(DFenetre f){
 		fenetre = f;
 	}
-	
-	public void windowClosing(WindowEvent e){
-		System.exit(0);
-	}
-	
-	public void windowIconified(WindowEvent e){
-		fenetre.pauseChrono();
-	}
-	
-	public void windowDeiconified(WindowEvent e){
-		fenetre.repriseChrono();
-	}
-	
-	
 }
