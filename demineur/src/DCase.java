565c85b70b790a801d535850ca8f88dec42da9f9
Bervas Yahn <bervasyahn@gmail.com>
2022-11-30 09:53:02 +0100

    rebase 1/1

--- demineur/src/DCase.java
+++ demineur/src/DCase.java
@@ -1,63 +1,61 @@
-public class DCase{
-	
+public class DCase {
+
 	private boolean mine;
 	private int mimesAlentour;
 	private boolean drapeau;
 	private boolean decouverte;
 	private boolean selection;
-	
-	public DCase(){
+
+	public DCase() {
 		mine = false;
 		decouverte = false;
 		drapeau = false;
 		selection = false;
 		mimesAlentour = -1;
 	}
-	
-	
-	public boolean estMine(){
+
+	public boolean estMine() {
 		return mine;
 	}
-	
-	public int getMinesAlentour(){		
+
+	public int getMinesAlentour() {
 		return mimesAlentour;
 	}
-	
-	public boolean yaDrapreau(){
+
+	public boolean yaDrapreau() {
 		return drapeau;
 	}
-	
-	public boolean estDecouverte(){
+
+	public boolean estDecouverte() {
 		return decouverte;
 	}
-		
-	public void setDecouverte(){
+
+	public void setDecouverte() {
 		decouverte = true;
 	}
-	
-	public void selectionner(){
+
+	public void selectionner() {
 		selection = true;
 	}
-	
-	public void deselectionner(){
+
+	public void deselectionner() {
 		selection = false;
 	}
-	
-	public boolean select(){
+
+	public boolean select() {
 		return selection;
 	}
-	
-	public void drapeauAction(){
-		if(!decouverte)
+
+	public void drapeauAction() {
+		if (!decouverte)
 			drapeau = (!drapeau);
-			
 	}
-	
-	public void poserBombe(){
+
+	public void poserBombe() {
 		mine = true;
 	}
-	
-	public void setMinesAlentour(int nb){
+
+	public void setMinesAlentour(int nb) {
 		mimesAlentour = nb;
 	}
 }
